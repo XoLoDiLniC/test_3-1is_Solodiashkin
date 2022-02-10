aaefb410b03723f94326d223ee535095845d3cf0
XoLoDiLniC <99400502+XoLoDiLniC@users.noreply.github.com>
2022-02-10 14:23:41 +0300

    XoLoDiLnIk
    good work

---
+++ LINQ-next/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_next
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+            var selectedTeams = teams.Where(t => t.ToUpper().StartsWith("Б")).OrderBy(t => t);
+
+            foreach (string s in selectedTeams)
+                Console.WriteLine(s);
+            Console.ReadKey();
+
+        }
+    }
+}
